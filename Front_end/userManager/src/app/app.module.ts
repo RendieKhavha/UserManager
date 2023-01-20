e28349d24a31f6800f2bb91590e3faa0222c272a
Khavharendwe Nemangaya <kdnemangaya@gmail.com>
2023-01-20 13:32:10 +0200

    frontend

---
+++ Front_end/userManager/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentDashboardComponent,
+    AdminDashboardComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
